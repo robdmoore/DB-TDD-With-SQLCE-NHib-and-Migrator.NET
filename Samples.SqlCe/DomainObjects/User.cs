afbd0507146c3023d463e0d22523e1c10c0b2293
Robert Moore <me@robdmoore.id.au>
2011-03-21 23:29:54 +0800

    Changing the naming and coding conventions used in User.cs and UserRepositoryTestCases.cs

--- Samples.SqlCe/DomainObjects/User.cs
+++ Samples.SqlCe/DomainObjects/User.cs
@@ -2,8 +2,8 @@ namespace Samples.SqlCe.DomainObjects {
     public class User {
         public virtual int Id { get; set; }
         public virtual string Username { get; set; }
-        public virtual string Firstname { get; set; }
-        public virtual string Lastname { get; set; }
-        public virtual string Email { get; set; }
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual string EmailAddress { get; set; }
     }
 }
--- Samples.SqlCe/Tests/UserRepositoryTestCases.cs
+++ Samples.SqlCe/Tests/UserRepositoryTestCases.cs
@@ -1,65 +1,69 @@
 using NHibernate;
 using NUnit.Framework;
 using Samples.SqlCe.DomainObjects;
+using Samples.SqlCe.Migrations;
 using Samples.SqlCe.Repositories;
 
 namespace Samples.SqlCe.Tests {
 
     [TestFixture]
-    public class UserRepositoryTestCases : EmbeddedTestBase {
-        private ISession session;
-        private UserRepository userRepository;
-        private User user;
+    public class UserRepositoryTestCases : EmbeddedTestBase
+    {
+        private ISession _session;
+        private UserRepository _userRepository;
+        private User _user;
 
-        private string firstname = "TestFN";
-        private string lastname = "TestLN";
-        private string email = "TestEmail";
-        private string username = "testusername";
+        private const string Firstname = "TestFN";
+        private const string Lastname = "TestLN";
+        private const string Email = "TestEmail";
+        private const string Username = "TestUserName";
 
         [TestFixtureSetUp]
-        public void TestFixtureSetUp() {
+        public void TestFixtureSetUp()
+        {
             Initialize("DomainObjects", typeof(User).Assembly);
 
-            session = CreateSession();
-            userRepository = new UserRepository();
-            userRepository.session = session;
+            _session = CreateSession();
+            _userRepository = new UserRepository {session = _session};
         }
 
         [TestFixtureTearDown]
-        public void TestFixtureTearDown() {
-            session.Dispose();
+        public void TestFixtureTearDown()
+        {
+            _session.Dispose();
         }
 
         [SetUp]
-        public void SetUp() {
-            user = new User();
-            user.Firstname = firstname;
-            user.Lastname = lastname;
-            user.Email = email;
-            user.Username = username;
+        public void SetUp()
+        {
+            _user = new User {FirstName = Firstname, LastName = Lastname, EmailAddress = Email, Username = Username};
 
             // Persist user to DB for testing
-            session.Save(user);
-            session.Flush();
-            session.Evict(user);
+            _session.Save(_user);
+            _session.Flush();
+            _session.Evict(_user);
         }
 
         [TearDown]
-        public void TearDown() {
-            user = null;
+        public void TearDown()
+        {
+            _user = null;
         }
 
         [Test]
-        public void CanSaveAndLoadUser() {
-            User loaded = session.Load<User>(user.Id);
-            Assert.AreEqual(user.Firstname, loaded.Firstname, "Firstname does not match");
-            Assert.AreEqual(user.Lastname, loaded.Lastname, "Lastname does not match");
-            Assert.AreEqual(user.Email, loaded.Email, "Email does not match");
+        public void Can_save_and_load_user()
+        {
+            var loaded = _session.Load<User>(_user.Id);
+            Assert.AreEqual(_user.Username, loaded.Username, "Username does not match");
+            Assert.AreEqual(_user.FirstName, loaded.FirstName, "FirstName does not match");
+            Assert.AreEqual(_user.LastName, loaded.LastName, "LastName does not match");
+            Assert.AreEqual(_user.EmailAddress, loaded.EmailAddress, "EmailAddress does not match");
         }
 
         [Test]
-        public void MakeSureUsernameIsAvailable() {
-            Assert.IsTrue(userRepository.CheckUsernameIsAvailable(username));
+        public void Make_sure_username_is_available()
+        {
+            Assert.IsTrue(_userRepository.CheckUsernameIsAvailable(Username));
         }
 
     }
